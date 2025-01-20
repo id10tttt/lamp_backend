b49179a61b394592bae53e83c4d63d3c4930f7af
1di0t <dgqcjx@gmail.com>
2025-01-20 14:43:31 +0800

    check right

---
+++ custom_addons/lamp_api/models/ir_binary.py
@@ -0,0 +1,13 @@
+﻿# -*- coding: utf-8 -*-
+from odoo import models
+
+IGNORE_ACCESS_RIGHT_CHECK = ['product.template', 'product.product']
+
+
+class IrBinary(models.AbstractModel):
+    _inherit = 'ir.binary'
+
+    def _find_record_check_access(self, record, access_token):
+        if record._name in IGNORE_ACCESS_RIGHT_CHECK:
+            return record
+        return super()._find_record_check_access(record, access_token)
