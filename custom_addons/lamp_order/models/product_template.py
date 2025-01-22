0b9d1ef976ae06c73781542b25d2b7bef68eb8f0
1di0t <dgqcjx@gmail.com>
2025-01-22 13:40:07 +0800

    允许翻译

--- custom_addons/lamp_order/models/product_template.py
+++ custom_addons/lamp_order/models/product_template.py
@@ -6,17 +6,17 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     rental = fields.Float('Rental', digits=(16, 2))
-    device_code = fields.Char('Device code')
+    device_code = fields.Char('Device code', translate=True)
     delay_price = fields.Float('Delay Price', digits=(16, 2))
     coupon = fields.Boolean('Coupon', default=False)
-    package_type = fields.Char('Package Type')
+    package_type = fields.Char('Package Type', translate=True)
 
     net_weight = fields.Float('Net Weight')
     gross_weight = fields.Float('Gross Weight')
 
-    logo_size = fields.Char('Logo Size')
-    product_size = fields.Char('Product Size')
-    packaging_size = fields.Char('Packaging Size')
+    logo_size = fields.Char('Logo Size', translate=True)
+    product_size = fields.Char('Product Size', translate=True)
+    packaging_size = fields.Char('Packaging Size', translate=True)
     monthly_subscription = fields.Float('Monthly Subscription')
 
     vip_price = fields.Float('VIP Price', digits=(16, 2))
