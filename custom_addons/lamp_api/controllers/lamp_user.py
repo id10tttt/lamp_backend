c4bb31cb760e29779e1bebf085bc7ba44b585817
1di0t <dgqcjx@gmail.com>
2025-02-05 11:00:49 +0800

    邮件认证

--- custom_addons/lamp_api/controllers/lamp_user.py
+++ custom_addons/lamp_api/controllers/lamp_user.py
@@ -51,58 +51,74 @@ class LAMPUser(http.Controller, BaseController):
         auth_type = payload_data.get('auth_type')
         email = payload_data.get('email')
 
+        redis_key = None
         if auth_type == 'mobile':
             if not mobile:
                 return self.response_http_json_error(400, message='请输入手机号!')
+            redis_key = mobile
+        elif auth_type == 'email':
+            if not email:
+                return self.response_http_json_error(400, message='请输入邮箱!')
+            redis_key = email
+        else:
+            return self.response_http_json_error(400, message='暂不支持该类型的认证!')
 
-            try:
-                send_count = self.get_mobile_sms_log_from_redis(mobile)
-                if not send_count:
-                    send_count = 0
-                else:
-                    send_count = send_count.decode()
-                    send_count = int(send_count)
-                    _logger.info('{} 发送短信次数: {}'.format(mobile, send_count))
-                cache_code = self.get_sms_code_from_redis(mobile)
-            except Exception as e:
-                _logger.error('出现错误: {}'.format(e))
-                cache_code = None
+        if not redis_key:
+            return self.response_http_json_error(400, message='认证数据类型异常!仅支持手机号/邮箱')
+
+        try:
+            send_count = self.get_mobile_sms_log_from_redis(redis_key)
+            if not send_count:
                 send_count = 0
+            else:
+                send_count = send_count.decode()
+                send_count = int(send_count)
+                _logger.info('{} 发送验证码次数: {}'.format(redis_key, send_count))
+            cache_code = self.get_sms_code_from_redis(redis_key)
+        except Exception as e:
+            _logger.error('出现错误: {}'.format(e))
+            cache_code = None
+            send_count = 0
 
-            if cache_code:
-                return self.response_http_json_error(400, message='请不要重复点击发送短信!')
+        if cache_code:
+            return self.response_http_json_error(400, message='请不要重复点击发送验证码!')
 
-            if send_count > MAX_MOBILE_SMS_LIMIT:
-                return self.response_http_json_error(400, message='超出每日发送限制，请稍后重试!')
+        if send_count > MAX_MOBILE_SMS_LIMIT:
+            return self.response_http_json_error(400, message='超出每日发送限制，请稍后重试!')
 
-            sms_code = get_random_login_code()
+        sms_code = get_random_login_code()
 
+        if auth_type == 'mobile':
             # send_state = AliCloudSMS().send_sms_code(phone_number=mobile, code=sms_code)
             send_state = send_sms_code(mobile, sms_code)
 
             # force_back = False
             if not send_state:
                 return self.response_http_json_error(400, message='发送短信失败!')
-
-            # 保存SMS CODE
-            self.save_sms_code_to_redis(mobile, sms_code)
-            self.save_mobile_sms_log_to_redis(mobile)
-
-            # 测试环境，接口返回code
-            prod_env = config.get('prod_env', False)
-            # prod_env = True
-
-            _logger.info('发送验证码: {}, {}'.format(mobile, sms_code))
-            resp_data = {
-                'code': sms_code
-            }
-
-            return self.response_http_json_success(message='发送成功!', data={} if prod_env else resp_data)
-
         elif auth_type == 'email':
-            pass
-        else:
-            return self.response_http_json_error(400, message='暂不支持该类型的认证!')
+            server_id = request.env['ir.mail_server'].sudo().search([], limit=1)
+            mail = request.env['mail.mail'].sudo().create({
+                'subject': 'Register/Loin Code',
+                'body_html': '<p>Dear Sir/Madam：</p><br/>Your code is :{}'.format(sms_code),
+                'email_to': '{}'.format(email),
+                'email_from': server_id.smtp_user if server_id else False,
+            })
+            mail.send()
+
+        # 保存SMS CODE
+        self.save_sms_code_to_redis(redis_key, sms_code)
+        self.save_mobile_sms_log_to_redis(redis_key)
+
+        # 测试环境，接口返回code
+        prod_env = config.get('prod_env', False)
+        # prod_env = True
+
+        _logger.info('发送验证码: {}, {}'.format(redis_key, sms_code))
+        resp_data = {
+            'code': sms_code
+        }
+
+        return self.response_http_json_success(message='发送成功!', data={} if prod_env else resp_data)
 
     @http.route('/api/v1/lamp/user/login', auth='public', methods=['POST'], csrf=False, cors="*", type='json')
     def lamp_user_login(self, lang='en_US', **kwargs):
@@ -116,30 +132,45 @@ class LAMPUser(http.Controller, BaseController):
             return self.response_http_json_error(400, message='出现错误!{}'.format(e))
 
         mobile = payload_data.get('mobile')
+        email = payload_data.get('email')
+        auth_type = payload_data.get('auth_type')
         code = payload_data.get('code')
 
-        if not all([mobile, code]):
-            return self.response_http_json_error(400, message='验证码错误!')
+        redis_key = None
+        if auth_type == 'mobile':
+            if not all([mobile, code]):
+                return self.response_http_json_error(400, message='验证码错误!')
+            redis_key = mobile
+        elif auth_type == 'email':
+            if not all([email, code]):
+                return self.response_http_json_error(400, message='验证码错误!')
+            redis_key = email
+        else:
+            return self.response_http_json_error(400, message='暂不支持该类型的认证!')
 
-        redis_code = self.get_sms_code_from_redis(mobile)
+        redis_code = self.get_sms_code_from_redis(redis_key)
 
         if not redis_code:
             return self.response_http_json_error(400, message='请先获取验证码!')
 
-        partner_id = self.get_or_create_res_partner(mobile)
+        if auth_type == 'email':
+            partner_id = self.get_or_create_res_partner_by_email(redis_key)
+        else:
+            partner_id = self.get_or_create_res_partner(redis_key)
 
         if not partner_id:
             return self.response_http_json_error(400, message='数据异常，请检查数据!!')
 
         payload_data = {
             'mobile': mobile,
+            'email': email,
             'uid': partner_id.id,
             'aud': LAMP_AUDIENCE,
             'iss': LAMP_ISSUER
         }
         jwt_token = jwt_encode(payload_data, DEFAULT_TOKEN_EXPIRE)
 
-        save_access_token_to_redis(mobile, jwt_token)
+        save_access_token_to_redis(redis_key, jwt_token)
 
         token_data = {
             'access_token': jwt_token,
