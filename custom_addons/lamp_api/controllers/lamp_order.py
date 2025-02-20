ea3aca129eda9dbed70f4a93588431be30995d14
1di0t <dgqcjx@gmail.com>
2025-02-20 17:09:04 +0800

    优化

--- custom_addons/lamp_api/controllers/lamp_order.py
+++ custom_addons/lamp_api/controllers/lamp_order.py
@@ -225,7 +225,7 @@ class SaleOrder(http.Controller, BaseController):
         if coupon_ids:
             filter_domain = [('id', 'in', coupon_ids),
                              ('partner_id', '=', request.partner_id)]
-            order_domain = ['|', ('order_id', '=', False), ('order_id.state', '!=', 'cancel')]
+            order_domain = [('order_id', '=', False)]
 
             filter_domain = expression.AND([filter_domain, order_domain])
             coupon_ids = request.env['coupon.coupon'].sudo().search(filter_domain)
