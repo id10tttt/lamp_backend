0e7163b8e7483f4cde8b6508228c71c0e1848d66
1di0t <dgqcjx@gmail.com>
2025-02-24 14:53:43 +0800

    优化

--- custom_addons/lamp_order/models/sale_order.py
+++ custom_addons/lamp_order/models/sale_order.py
@@ -70,6 +70,21 @@ class SaleOrder(models.Model):
     reward_amount = fields.Float("Reward Amount")
     redeem_amount = fields.Float('Redeem Amount')
 
+    @api.depends('order_line.invoice_lines')
+    def _get_invoiced(self):
+        # The invoice_ids are obtained thanks to the invoice lines of the SO
+        # lines, and we also search for possible refunds created directly from
+        # existing invoices. This is necessary since such a refund is not
+        # directly linked to the SO.
+        for order in self:
+            invoices = order.order_line.invoice_lines.move_id.filtered(
+                lambda r: r.move_type in ('out_invoice', 'out_refund'))
+            order.invoice_ids = invoices
+            order.invoice_count = len(invoices)
+
+            if all([invoice.payment_state == 'paid' for invoice in invoices]):
+                order.payment_status = '30'
+
     # 确认付款单
     def create_invoice_pay_now(self):
         self.ensure_one()
