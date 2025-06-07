1433c32b6835fe5a185e622404552304a69a9f4d
1di0t <dgqcjx@gmail.com>
2025-06-07 10:53:02 +0800

    优化库存的筛选

--- custom_addons/lamp_order/models/stock_quant.py
+++ custom_addons/lamp_order/models/stock_quant.py
@@ -5,4 +5,4 @@ from odoo import models, fields
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    warehouse_id = fields.Many2one('stock.warehouse', string='仓库', related='location_id.warehouse_id')
+    warehouse_id = fields.Many2one('stock.warehouse', string='仓库', related='location_id.warehouse_id', store=True)
